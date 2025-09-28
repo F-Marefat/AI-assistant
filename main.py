c8b70cb95b479d4de0f21bb04b3a538ef8fcdc12
unknown <marefat.ftm@gmail.com>
2025-09-28 19:20:02 +0330

    Initial commit: setup project stucture and first script

---
+++ main.py
@@ -0,0 +1,8 @@
+student_name = "fateme"
+
+print("hello, world!")
+print(f"welcome, {student_name}!")
+
+task_for_today = ["first code", "virtual env" , "install python"]
+print("today works:")
+print(task_for_today)
